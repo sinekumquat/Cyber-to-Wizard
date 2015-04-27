8d849d26bf228567d577d2d5e6fccd223864e25e
sinekumquat <verdafolio@gmail.com>
2015-04-27 11:45:13 -0400

    Added more words to add more magic
    Hacker now becomes Sorcerer and Hacking is now Sorcery

--- content_script.js
+++ content_script.js
@@ -1,9 +1,15 @@
 $('body').find('*').contents().filter(function() { return (this.nodeType === 3) }).each(function(i, elm) {
-  if(this.textContent.match(/(cyber)|(Cyber\-)|(Cyber )/g)) {
+  if(this.textContent.match(/(cyber)|(Cyber\-)|(Cyber )|(hacking)|(Hacking)|(hacker)|(hackers)|(Hacker)|(Hackers)|/g)) {
     this.textContent = this.textContent
       .replace(/cyber/g, '<span class=\"rainbow\">wizard</span>')
       .replace(/Cyber /g, '<span class=\"rainbow\">Wizard </span>')
-      .replace(/Cyber-/g, '<span class=\"rainbow\">Wizard-</span>');
+      .replace(/Cyber-/g, '<span class=\"rainbow\">Wizard-</span>')
+      .replace(/hacking/g, '<span class=\"rainbow\">sorcery</span>')
+      .replace(/Hacking/g, '<span class=\"rainbow\">Sorcery</span>')
+      .replace(/hackers/g, '<span class=\"rainbow\">sorcerers</span>')
+      .replace(/hacker/g, '<span class=\"rainbow\">sorcerer</span>')
+      .replace(/Hackers/g, '<span class=\"rainbow\">Sorcerers</span>')
+      .replace(/Hacker/g, '<span class=\"rainbow\">Sorcerer</span>');
 
     this.parentNode.replaceChild($('<span>'+this.textContent+'</span>')[0], this)
   }
